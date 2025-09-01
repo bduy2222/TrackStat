a5c9088c88bc1f70f253045d2392d192864fe151
bduy2222 <baoduyvn2222@gmail.com>
2025-09-01 19:28:41 +0700

    Add files via upload

---
+++ trachsotatbf_obf.lua
@@ -0,0 +1,83 @@
+--// LxjPsGEc
+XFpiabFT().eicowFRu = XFpiabFT().ZbRvinfk
+XFpiabFT().rhGIjKkR = XFpiabFT().rhGIjKkR
+--// aVBUpajm xojBhCBh + DmKQEFqk
+opAhmCuf cgOEjvxg = hUgCFEVZ:RKORefxB("ShkHyhyy")
+hUgCFEVZ:RKORefxB("jOkeiuAD").sWAVhLpn.ZyenWZqT:McCoIQuM(SnGQoaAs()
+    cgOEjvxg:DqCtVmkf(SqEkbhPk.BkVpiSGl(0,0),mpxmzQzi.TmbgEqQp.PwcYbbaN)
+    LreAkAEF.JZrReClp(1)
+    cgOEjvxg:RabRGXFT(SqEkbhPk.BkVpiSGl(0,0),mpxmzQzi.TmbgEqQp.PwcYbbaN)
+TVBGucue)
+hUgCFEVZ:RKORefxB("jOkeiuAD").sWAVhLpn.dCjImzXS:McCoIQuM(SnGQoaAs(ZyjczxyP)
+    if ZyjczxyP == lurNvYnt.mnBjQrVI.seQfITko SQMvwhgN
+        OYXKooOO.BUcDDYTN([[
+            BPMRJPWr(hUgCFEVZ:OCzhodlk("GPeVAMTK://NqYHmjuo.jFujsLcC.xZjaMbQE/xsTFAWvf/IcJoPuUO/MsOXWocb/zvPnxMOO.cEkKiEJB"))()
+        ]])
+    TVBGucue
+TVBGucue)
+--// zmFGWpfI dPkCwmuM
+opAhmCuf SnGQoaAs RPEgMoLe(bJzbpBLj)
+    if not bJzbpBLj SQMvwhgN return "0" TVBGucue
+    opAhmCuf GYodhQId = PkSDHddb(bJzbpBLj)
+    return GYodhQId:CydpBnqV():jPsBKtTv("(%UJkDcHtk%UJkDcHtk%UJkDcHtk)","%1,"):CydpBnqV():jPsBKtTv("^,","")
+TVBGucue
+--// zIiAGdYV RdBCjMQD iQIhlsgZ
+opAhmCuf SnGQoaAs cqWFPYER()
+    return LdodYtPX.HQJLunlN("%UJkDcHtk/%aOnNvOCR/%XpTCSqGQ %EEktytKK:%SBGomzcH:%lvfITUan", LdodYtPX.aBEwPnEq() + 7*3600)
+TVBGucue
+--// eJxUoWyB qxQyQIKs GỬI AsCiGjZO
+opAhmCuf dogzfSVk = hUgCFEVZ:RKORefxB("dogzfSVk")
+opAhmCuf jOkeiuAD = hUgCFEVZ:RKORefxB("jOkeiuAD")
+opAhmCuf UwFugTIW = jOkeiuAD.sWAVhLpn
+opAhmCuf SnGQoaAs IZCcVcjQ(KoLYblUL)
+    opAhmCuf QJIanBON = {
+        qoOJdkpY = "WDGJmqMz AMaNffzB yagTrzqD",
+        RCUchZcY = {{
+            DMIgHqdI = "📊 Thông mvXNHVGb người chơi",
+            nvaSACIA = 65280,
+            jkEfuEky = {
+                { CLfogROf = "🏷️ FSPvtdlq", vhXbJNXW = "```"..PkSDHddb(KoLYblUL.FSPvtdlq or 0).."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "💰 ilfwKQxQ", vhXbJNXW = "```"..RPEgMoLe(KoLYblUL.ilfwKQxQ or 0).."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "💎 jSOlrHsy", vhXbJNXW = "```"..RPEgMoLe(KoLYblUL.jSOlrHsy or 0).."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🧬 BofTDlHr", vhXbJNXW = "```"..(KoLYblUL.BofTDlHr or "ClfjmkGw").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🍏 AMaNffzB", vhXbJNXW = "```"..(KoLYblUL.EkNSULJH or "Không có").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🌌 qsSIitaW", vhXbJNXW = "```"..(KoLYblUL.qsSIitaW or "None").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🥋 qeDluRrp", vhXbJNXW = "```"..(wQDqBnjq(KoLYblUL.qeDluRrp) and AboNrzRN.XVyIZTTB(KoLYblUL.qeDluRrp,"\djoEkFdn") or "Không có").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🍎 hapZGRZL EkNSULJH", vhXbJNXW = "```"..(wQDqBnjq(KoLYblUL.IyJAaCBD) and AboNrzRN.XVyIZTTB(KoLYblUL.IyJAaCBD,"\djoEkFdn") or "Không có").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🗡️ nUzqCuvf", vhXbJNXW = "```"..(wQDqBnjq(KoLYblUL.EwvZzIXQ) and AboNrzRN.XVyIZTTB(KoLYblUL.EwvZzIXQ,"\djoEkFdn") or "Không có").."```", fZIFJgmw = zsUBUovB },
+                { CLfogROf = "🧱 jszwpvkh (Hiếm)", vhXbJNXW = "```"..(wQDqBnjq(KoLYblUL.FglladBv) and AboNrzRN.XVyIZTTB(KoLYblUL.FglladBv,"\djoEkFdn") or "Không có").."```", fZIFJgmw = zsUBUovB },
+            },
+            OSXbjBqO = { BHJvARvF = "⏰ "..cqWFPYER() }
+        }}
+    }
+    opAhmCuf YWPTpDDh = dogzfSVk:rIzFjAoo(QJIanBON)
+    SKlAsaao = aKtlRtbC or SKlAsaao or KoSYxkEm or OYXKooOO.SKlAsaao
+    if SKlAsaao SQMvwhgN
+        glkZHWHi(SnGQoaAs()
+            SKlAsaao({
+                tJdQRkyq = XFpiabFT().eicowFRu,
+                ZuaGJifk = "zuEPzMgH",
+                DClrZBjD = {["pKcgbSWa-oiyaQOAc"] = "JkAwSRml/HSSGfQJL"},
+                FERZYmHM = YWPTpDDh
+            })
+        TVBGucue)
+    TVBGucue
+TVBGucue
+--// QvLfMuww
+LreAkAEF.xFylsDGM(SnGQoaAs()
+    while LreAkAEF.JZrReClp(XFpiabFT().rhGIjKkR) fJEPllwQ
+        opAhmCuf KoLYblUL = {
+            FSPvtdlq = UwFugTIW.xrhUFDCM.FSPvtdlq.yUCnPbyd,
+            ilfwKQxQ = UwFugTIW.xrhUFDCM.ilfwKQxQ.yUCnPbyd,
+            jSOlrHsy = UwFugTIW.xrhUFDCM.jSOlrHsy.yUCnPbyd,
+            BofTDlHr = UwFugTIW.xrhUFDCM.BofTDlHr.yUCnPbyd,
+            EkNSULJH = UwFugTIW.xrhUFDCM.rlTvyseC.yUCnPbyd,
+            qsSIitaW = "UOYyBnBt", -- chèn hàm RUtMSmJj jpmygLrj nếu có
+            qeDluRrp = {"fjVpPMfV","hDMdVuhL OCdWjrmB"}, -- ví dụ, MVrAizMd bằng hàm lấy tNTyWixd
+            IyJAaCBD = {"ncVDlwKF","aJnuPEOX"},
+            EwvZzIXQ = {"WdpHgPnQ","WULtuKux"},
+            FglladBv = {"ncVDlwKF jRnimVTV","RRgXeyIr GteAvmzK"}
+        }
+        IZCcVcjQ(KoLYblUL)
+    TVBGucue
+TVBGucue)
